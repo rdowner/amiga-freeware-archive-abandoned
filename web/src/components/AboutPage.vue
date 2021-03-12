40bd7f9280840e4102716207ae192c492b7adf89
Richard Downer <richard@apache.org>
2021-03-12 21:09:20 +0000

    Fill in the About page

--- web/src/components/AboutPage.vue
+++ web/src/components/AboutPage.vue
@@ -1,7 +1,132 @@
 <template>
     <div>
-        <h1>About</h1>
-        <p>content</p>
+        <h1 class="display-1 text-center">About</h1>
+        <p class="lead text-center">
+            The Amiga Freeware Archive aims to collect, catalog, and make available for search and download,<br/>
+            the content of PD Libraries for classic Amiga computers.
+        </p>
+
+        <h2>A brief history of the Amiga computer</h2>
+        <p>
+            The Amiga series of home computers was launched in 1985 with the Amiga 1000, a revolutionary desktop
+            computer which was fully 16-bit and featured a modern graphics-and-mouse based user interface with a
+            multi-tasking operating system, and advanced (for the time) graphics and sound. Nothing like it had ever
+            been seen before at that price point ($1,295). It was followed in 1987 by the Amiga 500, an all-in-one
+            computer at a lower cost that bought the computer into the homes of millions throughout the late 80s and
+            earlier 90s. However Commodore mismanagement brought the company to liquidation in 1994. Amiga technology
+            continued for a period after under new owners, but by that time the computer industry had moved on and
+            standardised on Microsoft Windows, and the commercial life of Amiga came to an end during the 1990s.
+        </p>
+        <p>
+            It was, however, survived by a strong hobbyist scene. Cloanto's Amiga Forever product allowed
+            legally-licensed emulation using the original Amiga ROMs and disks, Toni Willen has developed the WinUAE
+            emulation software to cover a huge range of Amiga and third-party hardware accurately, and software
+            developers have continued to write software that runs on the original and emulated hardware. With the retro
+            computing scene developing throughout the 2000s and 2010s, the Amiga was kept alive. Thanks to the dropping
+            prices of custom hardware development and the advent of crowdfunding, there is even new Amiga hardware being
+            produced by the surviving Amiga community.
+        </p>
+
+        <h2>PD Libraries</h2>
+        <p>
+            During the Amiga's peak either side of 1990, the Internet was rarely available to home users. Instead, other
+            distribution channels for software appeared. There was obviously the commercial Amiga software which could
+            be brought in high street stores and by mail order. But there was also the PD Library - typically small
+            businesses or even individuals, who kept a "library" of disks that they would duplicate onto disk and send
+            out mail order for a small charge. Some of these became very big, such as 17 Bit Software, who created their
+            own disk library, and curated a community through their memberships and update disks. The other notable PD
+            Library was the "Amiga Library Disk" series curated by Fred Fish - commonly known as the Fish disks - which
+            were initially distributed by hand at Amiga user group meetings, but which were picked up and distributed by
+            the mail order PD libraries too.
+        </p>
+        <p>
+            PD stands for "public domain", but strictly speaking this was not accurate. In legal terms, "public domain"
+            refers to a work that is no longer covered by copyright (or the copyright holder has rescinded the
+            copyright and dedicated the work to the public domain). However most of the material in PD libraries was
+            copyrighted but intended to be freely-distributed. Some works were <em>Shareware</em>, where you were
+            allowed to use the software but if you used beyond an initial period, you were expected to send money to the
+            author. There were also some variations such as beerware or postcardware! You could also find demo versions
+            of commercial software in PD library collections, too.
+        </p>
+
+        <h2>About this project</h2>
+        <p>
+            Throughout the decades since the Amiga has faded, obviously the original companies operating the PD
+            libraries have disappeared. Some of the largest collections have survived and are still ubiquitous - for
+            example, it doesn't take much searching on the Internet to find a CD-ROM containing disk images of the
+            17 Bit Software library. However these collections are not the easiest to use - sometimes the CD-ROM images
+            can be read on a modern computer, but sometimes they expect to be run on an actual Amiga. Sometimes it
+            requires knowledge of the Amiga operating system to extract and use images, which you may have forgotten
+            about in the 20 years since you last used an Amiga! And it can be a significant challenge to move software
+            between your current PC, an emulated Amiga, and a real Amiga.
+        </p>
+        <p>
+            The goal of this project is:
+        </p>
+        <ul class="lead">
+            <li>To collect and rebuild as many of the original Amiga PD Library collections as possible;</li>
+            <li>To free downloads, in convenient and accessible formats, of the collected software; and</li>
+            <li>To catalog the collections for browsing and searching</li>
+        </ul>
+
+        <h2>Frequently Asked Questions</h2>
+        <h3 class="text-secondary">Is it really free?</h3>
+        <p>Yes, this service is offered free of charge for all who do not abuse it.</p>
+
+        <h3 class="text-secondary">Can I download as much as I like?</h3>
+        <p>Yes, as long as you still in compliance with <router-link to="/about/acceptable-use-policy">the Acceptable
+        Use Policy</router-link>. Basically, as long as you are using this website by hand, and are not attempting to
+        scrape or automatically download amounts of content, you can download as much as you like.</p>
+
+        <h3 class="text-secondary">Can I download the full archive?</h3>
+        <p>Yes, but only if you use the <router-link to="/about/bulk-downloads">bulk download interface</router-link>.
+        Using a scraper or automatic tool directly on the website is not permitted as it costs us too much, but by
+        using the bulk download interface you can efficiently obtain the entire website without a cost burden on the
+        site operators.</p>
+
+        <h3 class="text-secondary">How is does site differ from Aminet?</h3>
+        <p>Aminet is an amazing resource and contains a huge amount of Amiga software, but it only took off once
+        Internet access was starting to become cost-effective. Consequently it tends to focus on software that was
+        written in the latter half of the Amiga's commercial life. By contrast, this site is attempting to obtain PD
+        software from much earlier in the Amiga's history. Furthermore, Aminet continues to accept new submissions and
+        has done for several decades, whereas this site is more of a historical archive. We see the two as complementary
+        - Aminet is constantly growing with new software, whereas this site is attempting to be a historical
+        archive.</p>
+
+        <h3 class="text-secondary">Do you accept submissions?</h3>
+        <p>
+            We do not accept submissions of individual software packages - for that purpose, please consider Aminet.
+            However if you have PD library content that we do not currently carry (such as an archive of a lesser-known
+            collection), we may be very interested in evaluating it to be cataloged and added to the site!
+        </p>
+
+        <h3 class="text-secondary">What should I do if I find a mistake or a problem?</h3>
+        <p>
+            Please contact us so that we can investigate. As a historical archive it's important for us to archive the
+            broken items as well as the working ones - however it's important that the archive is usable and we
+            sometimes have the option of repairing and "remastering" items in the archive and offering them for download
+            along the original items.
+        </p>
+        <p>
+            Please remember that, particularly in the early days of the Amiga, not all software was reliable or
+            compatible with a wide range of machines - for example, some older software will crash on newer machines, or
+            vice versa. If you are having trouble getting a particular software package to run, please read its
+            requirements carefully and see if you can modify your Amiga environment to make it more compatible.
+        </p>
+
+        <h3 class="text-secondary">Who is behind this website?</h3>
+        <p>
+            The Amiga Freeware Archive was created by Richard Downer. Richard discovered by Amiga computer aged 11 in
+            1989. Already familiar with the Commodore 8-bit computers, the Amiga was a revelation and Richard set to
+            learning as much about it as possible. It kickstarted Richard's career in technology where he still works
+            today.
+        </p>
+
+        <h3 class="text-secondary">How can I contact the website operators?</h3>
+        <p>
+            Send an email to <a href="mailto:amigafreeware@frontier.town">amigafreeware@frontier.town</a>.
+        </p>
+
     </div>
 </template>
 
